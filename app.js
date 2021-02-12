b0d8d6c94e78335c91de11a1371a868f2f34ad72
johan <johanfelipe29@gmail.com>
2021-02-11 23:26:45 -0500

    Post request validations were implemented, verifies if the username and/or email already exist in the database

--- app.js
+++ app.js
@@ -24,26 +24,47 @@ app.get('/', (request, response) => {
 });
 
 app.post('/', (request, response) => {
-    //console.log(request.body);
+
     const { name, username, email, password } = request.body;
-    const newUser = new User({
-        name,
-        username,
-        email,
-        password
-    });
-    User.create({
-        name,
-        username,
-        email,
-        password
-    })
-    .then((user) => {
-        response.send(`${name} has been added to the database`);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+
+    //Query if username or email are already used
+    User.find({ $or: [ { username }, { email } ] })
+        .then((user) => {
+
+            //If the object is empty the registration is efectuated
+            if(Object.keys(user).length === 0) {
+                
+                User.create({
+                    name,
+                    username,
+                    email,
+                    password
+                })
+                .then((user) => {
+                    response.send(`${name} has been added to the database`);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+            } else {
+                //If two objects are founded that means the 
+                //username and email are already registered
+                if(user.length > 1) {
+                    response.send(`username and email already registered!`);
+                } else {
+                    //Verify either if the username is already registered or the email
+                    if(user[0].username === username) {
+                        response.send(`username already registered.`);
+                    } else {
+                        response.send(`email already registered.`);
+                    }
+                }
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
 
     //console.log(newUser);
     //response.send('Post request');
